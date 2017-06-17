90d25ef2af7760b661ab6229cb81d458db931970
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-06-16 23:29:21 -0500

    isolate bashisms

--- postgres.sh
+++ postgres.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -euo pipefail
 
 su postgres -c '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf'
